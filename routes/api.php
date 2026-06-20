eeca39a261eb1e64f55d045d823a6ded1bb9c512
M. Zidan Richal Fajril Falah <richalfajril@gmail.com>
2026-06-21 00:34:51 +0700

    refactor: reorder user management routes in api.php for better logical grouping

--- routes/api.php
+++ routes/api.php
@@ -117,10 +117,10 @@ Route::middleware(['auth:sanctum', 'admin'])
             ->except(['show']);
         Route::get('/users', [AdminUserController::class, 'index']);
         Route::get('/users/export', [AdminUserController::class, 'export']);
-        Route::get('/users/{user}', [AdminUserController::class, 'show']);
-        Route::delete('/users/{user}', [AdminUserController::class, 'destroy']);
         Route::get('/users/vip-preview', [AdminUserController::class, 'vipPreview']);
         Route::post('/users/inject-vip-tickets', [AdminUserController::class, 'injectVipTickets']);
+        Route::get('/users/{user}', [AdminUserController::class, 'show']);
+        Route::delete('/users/{user}', [AdminUserController::class, 'destroy']);
 
         // --- SUBTEST & MASTER SOAL ---
         Route::apiResource('subtests', SubtestController::class)->except(['index']);
